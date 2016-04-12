fed4fda9a9b4d0bb03d858ecace3576bf211897d
DawnChan <dcutilities@gmail.com>
2016-04-12 19:06:49 +1000

    add run_analysis

---
+++ run_analysis.R
@@ -0,0 +1,45 @@
+install.packages(data.table)
+
+
+
+# loading all files
+subjecttest <- read.table("~/UCI HAR Dataset/test/subject_test.txt")
+xtest <- read.table("~/UCI HAR Dataset/test/X_test.txt")
+ytest <- read.table("~/UCI HAR Dataset/test/y_test.txt")
+
+subjecttrain <- read.table("~/UCI HAR Dataset/train/subject_train.txt")
+xtrain <- read.table("~/UCI HAR Dataset/train/X_train.txt")
+ytrain <- read.table("~/UCI HAR Dataset/train/y_train.txt")
+
+features <- read.table("~/UCI HAR Dataset/features.txt", col.names = c("featureID", "featurelabel"))
+activity <- read.table("~/UCI HAR Dataset/activity_labels.txt", col.names = c("activityID", "activitylabel"))
+
+#row binding test and train data sets
+subject <- rbind(subjecttest, subjecttrain)
+xdata <- rbind(xtest, xtrain)
+ydata <- rbind(ytest, ytrain)
+
+#subset mean and std deviation columns
+msfeatures <- grep("-mean\\(\\)|-std\\(\\)", features$featurelabel)
+msxdata <- xdata[, msfeatures]
+
+#descriptive column name change for subject and ydata 
+names(subject) <- "subjectID"
+names(ydata) <- "activity"
+
+#descriptive column name change for msxdata 
+names(msxdata) <- features[msfeatures, 2]
+
+#descriptive activity name for ydata
+ydata[, 1] <- activity[ydata[, 1], 2]
+
+#column binding all data sets to create tidy dataset
+df <- cbind(subject, ydata, msxdata)
+
+#create second tidy dataset with average for each subject and activity
+dt <- data.table(df)
+meltdt <- melt(dt, id=c("subjectID", "activity"))
+finaldt <- dcast(meltdt, subjectID + activity~variable, mean)
+
+#write csv file of new dataset
+write.csv(finaldt, file = "finaldata.csv") 
